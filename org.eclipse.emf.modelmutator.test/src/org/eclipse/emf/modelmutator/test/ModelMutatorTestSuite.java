f0139c6d2a913c4ac8be96c9784c9087282db4e3
neobsen <neobsen@googlemail.com>
2011-12-06 17:41:31 +0100

    fixed naming, forgot this one ;)

--- org.eclipse.emf.modelmutator.test/src/org/eclipse/emf/modelmutator/test/ModelMutatorTestSuite.java
+++ org.eclipse.emf.modelmutator.test/src/org/eclipse/emf/modelmutator/test/ModelMutatorTestSuite.java
@@ -1,6 +1,6 @@
 package org.eclipse.emf.modelmutator.test;
 
-import org.eclipse.emf.modemutator.generator.ModelGeneratorTest;
+import org.eclipse.emf.modelmutator.generator.ModelGeneratorTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
